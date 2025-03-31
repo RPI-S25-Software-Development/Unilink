8cb6e485621847057ccdb5057ad5c2274993d8cd
dangb24 <112642702+dangb24@users.noreply.github.com>
2025-03-31 17:02:49 -0400

    change saves to likes

--- server/index.js
+++ server/index.js
@@ -37,13 +37,13 @@ const userRoutes = require('./routes/users');
 const eventsRoutes = require('./routes/events');
 const organizationsRoutes = require('./routes/organizations');
 const rsvpsRoutes = require('./routes/rsvps');
-const savesRoutes = require('./routes/saves');
+const likesRoutes = require('./routes/likes');
 const tagsRoutes = require('./routes/tags');
 app.use('/users', userRoutes);
 app.use('/events', eventsRoutes);
 app.use('/organizations', organizationsRoutes);
 app.use('/rsvps', rsvpsRoutes);
-app.use('/saves', savesRoutes);
+app.use('/likes', likesRoutes);
 app.use('/tags', tagsRoutes);
 
 app.get('/', (req, res) => {
