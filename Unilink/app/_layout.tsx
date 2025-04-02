fed6d7f5df71d1fae59e66309bf4f134bf2f83db
bajajb <bajajb@rpi.edu>
2025-04-02 09:27:40 -0400

    Children support for Banner
    Added children support for Banner, allowing for the text to be a Link

--- Unilink/app/_layout.tsx
+++ Unilink/app/_layout.tsx
@@ -1,8 +1,8 @@
-import { Stack } from "expo-router";
+import { Stack, Link } from "expo-router";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from 'react';
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { StatusBar } from 'expo-status-bar';
 
 import Banner from "@/components/Banner";
@@ -26,10 +26,18 @@ export default function RootLayout() {
 
   return (
     <View style={{flex: 1}}>
-      <Banner backgroundColor="#DC0000" titleColor="#FFFFFF" title="RPI Unilink"
-            titleFontFamily="Inter" titleBold/>
+      <Banner backgroundColor="#DC0000">
+        <Link href="/home" className="w-fit">
+          <Text style={{
+              color: "#FFFFFF",
+              fontSize: 20,
+              fontFamily: "Inter",
+              fontWeight: "bold",
+          }}>RPI Unilink</Text>
+        </Link>
+      </Banner>
       <Stack screenOptions={{headerShown: false, contentStyle: {backgroundColor: "white"}}}>
-        <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+        <Stack.Screen  name="(tabs)" options={{headerShown: false}}/>
         <Stack.Screen name="preferences" options={{headerShown: false}}/>
         <Stack.Screen name="+not-found" options={{headerShown: false}}/>
       </Stack>
