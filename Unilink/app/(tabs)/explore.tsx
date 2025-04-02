e371b8a1572e97ff0e6c0280c260c4c3f2e79999
bhavesh42 <bajajb@rpi.edu>
2025-04-02 14:07:21 -0400

    Initial Explore Page Design
    - Same event boxes as home
    - Added category boxes
    - Preliminary search bar box

--- Unilink/app/(tabs)/explore.tsx
+++ Unilink/app/(tabs)/explore.tsx
@@ -1,16 +1,34 @@
-import { Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import "@/global.css";
+
+import { PlaceHolderEvent } from "@/app/(tabs)/home";
+
+import RoundedBox from "@/components/RoundedBox";
+import MedButton from "@/components/MedButton";
+import HeaderText from "@/components/HeaderText";
 
 export default function ExploreScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Explore</Text>
-    </View>
-  );
+      <ScrollView className="flex-1">
+        <View className="items-center py-4">
+          <RoundedBox width="90%" height={10} className="my-3"/>
+          <View className="my-3">
+            <HeaderText fontSize={24}>Explore by Category</HeaderText>
+            <ScrollView horizontal style={{maxHeight: 75}}>
+              <View className="flex flex-row px-5">
+                <MedButton label="Academic" scale={0.9} baseFontSize={16}/>
+                <MedButton label="Sports" scale={0.9}baseFontSize={16}/>
+                <MedButton label="Club" scale={0.9} baseFontSize={16}/>
+                <MedButton label="Career" scale={0.9}baseFontSize={16}/>
+              </View>
+            </ScrollView>
+          </View>
+          {PlaceHolderEvent}
+          {PlaceHolderEvent}
+          {PlaceHolderEvent}
+          {PlaceHolderEvent}
+          {PlaceHolderEvent}
+        </View>
+      </ScrollView>
+    );
 }
--- Unilink/app/(tabs)/home.tsx
+++ Unilink/app/(tabs)/home.tsx
@@ -5,30 +5,30 @@ import "@/global.css";
 import EventBox from "@/components/EventBox";
 import MedButton from "@/components/MedButton";
 
-export default function HomeScreen() {
-  const PlaceholderImage = require('@/assets/images/LitterBug.png');
-  const EventTitle = "@SSTF";
-  const EventDescription = "Join us for the Litter Bug Battle!";
-  const PlaceHolderEvent = <EventBox
-    imageSource={PlaceholderImage}
-    eventText={{
-      tags: [
-        { backgroundColor: "#8FC9FF", name: "Clubs & Organizations" },
-        { backgroundColor: "#9FEA8E", name: "Volunteering" },
-        { backgroundColor: "#C98FFF", name: "Social Gatherings" }
-      ],
-      title: EventTitle, description: EventDescription,
-      details: [
-        { key: "location", iconSource: {evilIconName: "location"}, text: "'87 Gym" },
-        { key: "time", iconSource: {evilIconName: "clock"}, text: "Oct 27, 12-4" }
-      ]
-    }}
-    interactionCounts={{
-      likeCount: 42,
-      rsvpCount: 28
-    }}
-  />
+const PlaceholderImage = require('@/assets/images/LitterBug.png');
+const EventTitle = "@SSTF";
+const EventDescription = "Join us for the Litter Bug Battle!";
+export const PlaceHolderEvent = <EventBox
+  imageSource={PlaceholderImage}
+  eventText={{
+    tags: [
+      { backgroundColor: "#8FC9FF", name: "Clubs & Organizations" },
+      { backgroundColor: "#9FEA8E", name: "Volunteering" },
+      { backgroundColor: "#C98FFF", name: "Social Gatherings" }
+    ],
+    title: EventTitle, description: EventDescription,
+    details: [
+      { key: "location", iconSource: {evilIconName: "location"}, text: "'87 Gym" },
+      { key: "time", iconSource: {evilIconName: "clock"}, text: "Oct 27, 12-4" }
+    ]
+  }}
+  interactionCounts={{
+    likeCount: 42,
+    rsvpCount: 28
+  }}
+/>
 
+export default function HomeScreen() {
   return (
     <ScrollView className="flex-1">
       <View className="items-center py-4">
