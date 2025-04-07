501ae8110b9121d138cd6d9054cc5bae251d4049
carsoe2 <91489348+carsoe2@users.noreply.github.com>
2025-04-07 16:10:56 -0400

    creating dockerfile for server

--- server/index.js
+++ server/index.js
@@ -56,6 +56,6 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Server listening at https://${host}:${port}`);
 });
