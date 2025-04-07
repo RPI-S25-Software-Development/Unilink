7e1d7ab313e5236efd93469453c9e80d307a8f83
carsoe2 <91489348+carsoe2@users.noreply.github.com>
2025-04-07 14:55:26 -0400

    updating er diagram

--- server/scheduled_jobs.js
+++ server/scheduled_jobs.js
@@ -3,7 +3,7 @@ const crypto = require("crypto");
 const pool = require('./db');
 
 async function scheduledJobs() {    
-    // TASK 1: delete expired events in the last 24 hours
+    // Delete expired events in the last 24 hours
     // fetch all event_ids which expired in the last 24 hours
     const result = await pool.query("select event_id from unilink.events where expiration_date <= NOW() and expiration_date >= NOW() - INTERVAL '1 day'");
     for (const row of result.rows) {
@@ -15,21 +15,6 @@ async function scheduledJobs() {
         const query2 = `update unilink.rsvps set still_valid=$1 where event_id=$2`;
         const result2 = await pool.query(query2, [false, event_id]);
     }
-
-    // TASK 2: generate notifications for upcoming events
-    // fetch all event_ids coming up in the next 48 hours
-    // const result4 = await pool.query("select event_id, event_name, event_time from unilink.events where event_time >= NOW() and event_time <= NOW() + INTERVAL '2 days'");
-    // for (const row of result4.rows) {
-    //     const event_id = row['event_id'];
-    //     const event_name = row['event_name'];
-    //     const event_time = row['event_time'];
-    //     // check rsvps to retrieve users signed up for this event
-    //     const result5 = await pool.query(`select user_id from unilink.rsvps where event_id='${event_id} and still_valid='${true}`);
-    //     for (const user_id of result5.rows) {
-    //         // Create and send event notification
-    //         // Post notification entry to db?
-    //     }
-    // }
 }
 
 // Call API immediately, then every 24 hours (86400000 ms)
