8c77b3d2128dd73eb751f9e9d4289e1778df0fbf
bhavesh42 <bajajb@rpi.edu>
2025-04-09 16:03:16 -0400

    Data Driven Event List Component
    Made more fields of event list event components data driven, and added conversion function for event time API data

--- Unilink/components/EventsList.tsx
+++ Unilink/components/EventsList.tsx
@@ -6,7 +6,7 @@ type Props = {
 }
 
 function convertEventTagsAPIData(eventTagsAPIData: any[]) {
-  var result: EventTagProps[] = [];
+  var result = [];
 
   for(var eventTagAPIData of eventTagsAPIData) {
     result.push({
@@ -18,28 +18,60 @@ function convertEventTagsAPIData(eventTagsAPIData: any[]) {
   return result;
 };
 
-function eventAPIDataToComponent(eventAPIData: any,
-eventTagsAPIData: any[],
-eventOrganizationName: string) {
-  return (
+function convertEventTimeAPIData(eventTimeAPIData: string) {
+  var sampleTime = "2025-02-06T17:00:00.000Z";
+
+  var [date, time] = eventTimeAPIData.split("T");
+  time = time.split(".")[0];
+
+  const allMonths = new Map([
+    [1, "Jan"],
+    [2, "Feb"],
+    [3, "Mar"],
+    [4, "Apr"],
+    [5, "May"],
+    [6, "Jun"],
+    [7, "Jul"],
+    [8, "Aug"],
+    [9, "Sep"],
+    [10, "Oct"],
+    [11, "Nov"],
+    [12, "Feb"]
+  ]);
+
+  var [year, month, day] = date.split("-");
+  var [hour, minute, seconds] = time.split(":");
+
+  return allMonths.get(+month) + " " + day + ", " + hour + ":" + minute;
+}
+
+function eventAPIDataToComponent(eventsAPIData: any[]) {
+  var result = [];
+
+  for(var eventAPIData of eventsAPIData) {
+    result.push(
     <EventBox
       imageSource={require("@/assets/images/" + eventAPIData.poster_path)}
       eventText={{
-        tags: convertEventTagsAPIData(eventTagsAPIData),
+        tags: convertEventTagsAPIData(eventAPIData.event_tags),
         title: eventAPIData.title, description: eventAPIData.event_description,
         details: [
-          { key: "organization", iconSource: {evilIconName: "user"}, text: eventOrganizationName },
+          { key: "organization", iconSource: {evilIconName: "user"},
+          text: eventAPIData.organization_name },
           { key: "location", iconSource: {evilIconName: "location"},
           text: eventAPIData.event_location },
-          { key: "time", iconSource: {evilIconName: "clock"}, text: eventAPIData.event_time }
+          { key: "time", iconSource: {evilIconName: "clock"},
+          text: convertEventTimeAPIData(eventAPIData.event_time) }
         ]
       }}
       interactionCounts={{
-        likeCount: 42,
-        rsvpCount: 28
+        likeCount: eventAPIData.event_likes,
+        rsvpCount: eventAPIData.event_rsvps
       }}
-    />
-  )
+    />);
+  }
+
+  return result;
 };
 
 export default function EventsList({ eventsApiRoute }: Props) {
